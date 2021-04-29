11436654591dfab3569c7840b11815437558b5e0
Gaurav Lolge <guarav.lolge1@gmail.com>
2021-04-29 10:06:30 +0530

    Added security helper.

---
+++ CompanyName.MyAppName.WebApi/Common/Helpers/SecurityHelper.cs
@@ -0,0 +1,33 @@
+﻿using CompanyName.MyAppName.Infra;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace CompanyName.MyAppName.WebApi.Common.Helpers
+{
+    /// <summary>
+    /// provides variuos members to manage security of the application.
+    /// </summary>
+    public static class SecurityHelper
+    {
+        /// <summary>
+        /// Generates the json web token.
+        /// </summary>
+        /// <returns>Jwt Token</returns>
+        public static string GenerateJSONWebToken(IConfiguration config)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config[Constants.ConfigKeys.JWT_KEY]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(config[Constants.ConfigKeys.JWT_ISSUER],
+                                  config[Constants.ConfigKeys.JWT_ISSUER],
+                                  null,
+                                  expires: DateTime.Now.AddMinutes(Convert.ToInt32(config[Constants.ConfigKeys.JWT_EXPIRY_TIME])),
+                                  signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- CompanyName.MyAppName.WebApi/Controllers/AccountController.cs
+++ CompanyName.MyAppName.WebApi/Controllers/AccountController.cs
@@ -1,10 +1,6 @@
-﻿using CompanyName.MyAppName.Infra;
+﻿using CompanyName.MyAppName.WebApi.Common.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.IdentityModel.Tokens.Jwt;
-using System.Text;
 using Dm = CompanyName.MyAppName.Model.Models;
 
 namespace CompanyName.MyAppName.WebApi.Controllers
@@ -50,7 +46,7 @@ namespace CompanyName.MyAppName.WebApi.Controllers
 
             if ("" != null && IsValidUser(null))
             {
-                var authToken = GenerateJSONWebToken();
+                var authToken = SecurityHelper.GenerateJSONWebToken(config);
                 return Ok(new { authToken });
             }
 
@@ -76,24 +72,6 @@ namespace CompanyName.MyAppName.WebApi.Controllers
 
 
             return valid;
-        }
-
-        /// <summary>
-        /// Generates the json web token.
-        /// </summary>
-        /// <returns>Jwt Token</returns>
-        private string GenerateJSONWebToken()
-        {
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config[Constants.ConfigKeys.JWT_KEY]));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
-            var token = new JwtSecurityToken(config[Constants.ConfigKeys.JWT_ISSUER],
-                                  config[Constants.ConfigKeys.JWT_ISSUER],
-                                  null,
-                                  expires: DateTime.Now.AddMinutes(Convert.ToInt32(config[Constants.ConfigKeys.JWT_EXPIRY_TIME])),
-                                  signingCredentials: credentials);
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
         #endregion Private Methods
